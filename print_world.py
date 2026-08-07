379b4413feb5d7edc7c557921025b7fae51c2604
sepehrskysh20 <sepehr.sky.sh@gmail.com>
2026-08-07 18:01:58 +0200

    Adding updater feature

--- print_world.py
+++ print_world.py
@@ -1,24 +1,35 @@
+import time
 import random as rand
 
 ascii_deadOrAlive = {0 : "□",1 : "■"}
 
-world_test1 = []
 
-for i in range(10):
-    world_test1.append([])
-    for j in range(10):
-        world_test1[i].append(rand.randint(0, 1))
+def world_random_generator() -> list[list[int]]:
+    world_test1 = []
+    for i in range(10):
+        world_test1.append([])
+        for j in range(10):
+            world_test1[i].append(rand.randint(0, 1))
+    return world_test1
 
 world_test2 =  [[0, 0, 1],
                 [1, 0, 1],
                 [1, 1, 1]]
 
 def print_world(world:list[list[int]],
-                mode:int = 1) -> None:
+                update_mode:int = False) -> None:
+
     for i in range(len(world)):
         for j in range(len(world[0])):
             print(ascii_deadOrAlive[world[i][j]], " ", end="")
         print("")
+    
+    if (update_mode == True):
+        for _ in range(len(world)):
+            print("\033[F\033[K", end="")
+    
+for i in range(10):
+    world = world_random_generator()
+    print_world(world, update_mode = True)
+    time.sleep(0.5)
 
-
-print_world(world_test1)
